b62aa1b862a3c1499e6ef5432032766e066c3a2a
BlackMercy <pitawat.n@gmail.com>
2015-02-14 22:02:01 +0700

    Initial commit

---
+++ app/src/main/java/com/siit/pitawat/tappingmania/CountDown.java
@@ -0,0 +1,14 @@
+package com.siit.pitawat.tappingmania;
+
+import android.view.View;
+
+/**
+ * Created by Pitawat on 14/2/2015.
+ */
+public class CountDown {
+    int i;
+    public void timer (View v){
+        //TextView countdown = (TextView)findViewbyId(R.id.countdown);
+    }
+
+}
---
+++ app/src/main/java/com/siit/pitawat/tappingmania/Game.java
@@ -0,0 +1,8 @@
+package com.siit.pitawat.tappingmania;
+
+/**
+ * Created by Pitawat on 14/2/2015.
+ */
+public class Game {
+
+}
