607f745bacf1fe25c575b35366ad4486a9c16459
BlackMercy <pitawat.n@gmail.com>
2015-02-17 07:56:32 +0700

    Almost half way now. Two players can now compete.

--- app/src/main/java/com/siit/pitawat/tappingmania/Game.java
+++ app/src/main/java/com/siit/pitawat/tappingmania/Game.java
@@ -17,14 +17,31 @@ public class Game extends ActionBarActivity {
     }
 
     public void tap (View v){
-        TextView scoreP1 = (TextView) findViewById(R.id.scoreP1);
-        int sP1 = Integer.parseInt(scoreP1.toString());
+//        TextView scoreP1 = (TextView) findViewById(R.id.scoreP1);
+//        int sP1 = Integer.parseInt(scoreP1.toString());
+//
+//        int id = v.getId();
+//        switch (id) {
+//            case R.id.buttonP1:
+//                sP1++;
+//                scoreP1.setText(sP1);
+//        }
+        TextView scoreP1 = (TextView)findViewById(R.id.scoreP1);
+        TextView scoreP2 = (TextView)findViewById(R.id.scoreP2);
+        int sP1 = Integer.parseInt(scoreP1.getText().toString());
+        int sP2 = Integer.parseInt(scoreP2.getText().toString());
 
         int id = v.getId();
         switch (id) {
             case R.id.buttonP1:
                 sP1++;
-                scoreP1.setText(sP1);
+                scoreP1.setText(Integer.toString(sP1));
+                break;
+
+            case R.id.buttonP2:
+                sP2++;
+                scoreP2.setText(Integer.toString(sP2));
+                break;
         }
     }
 }
