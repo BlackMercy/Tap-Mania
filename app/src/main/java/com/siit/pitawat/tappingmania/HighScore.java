740134f1d3d47ab4e5a34d324805bd9d921cb7d6
BlackMercy <pitawat.n@gmail.com>
2015-04-29 20:08:49 +0700

    Submitted project code

--- app/src/main/java/com/siit/pitawat/tappingmania/HighScore.java
+++ app/src/main/java/com/siit/pitawat/tappingmania/HighScore.java
@@ -1,11 +1,9 @@
 package com.siit.pitawat.tappingmania;
 
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.ActionMode;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -22,6 +20,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Jatupat on 23-Feb-15.
@@ -29,13 +28,131 @@ import java.util.Map;
 
 public class HighScore extends ActionBarActivity {
     SimpleAdapter adapter2, adapter3, adapter4,adapter5;
-    ActionMode actionMode;
     ArrayList<Map<String, String>> data2,data3,data4,data5;
 
     JSONParser jp = new JSONParser();
 
 
-    //List<NameValuePair> parameters = new ArrayList<NameValuePair>();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.highscore);
+
+        data2 = new ArrayList<Map<String, String>>();
+
+//        Map<String, String> item = new HashMap<String, String>();
+//        item.put("1", "tttt");
+//        item.put("4", "xxxxx");
+//        Map<String, String> item2 = new HashMap<String, String>();
+//        item.put("2", "tttt");
+//        item.put("5", "xxxxx");
+//        Map<String, String> item3 = new HashMap<String, String>();
+//        item.put("3", "tttt");
+//        item.put("6", "xxxxx");
+//        data2.add(item);
+//        data2.add(item2);
+//        data2.add(item3);
+
+        data3 = new ArrayList<Map<String, String>>();
+        data4 = new ArrayList<Map<String, String>>();
+        data5 = new ArrayList<Map<String, String>>();
+        adapter2 = new SimpleAdapter(this,
+                data2,
+                android.R.layout.simple_list_item_2,
+                new String[]{"uname", "score"},
+                new int[]{android.R.id.text1, android.R.id.text2});
+
+        adapter3 = new SimpleAdapter(this,
+                data3,
+                android.R.layout.simple_list_item_2,
+                new String[]{"uname", "score"},
+                new int[]{android.R.id.text1, android.R.id.text2});
+        adapter4 = new SimpleAdapter(this,
+                data4,
+                android.R.layout.simple_list_item_2,
+                new String[]{"uname", "score"},
+                new int[]{android.R.id.text1, android.R.id.text2});
+        adapter5 = new SimpleAdapter(this,
+                data5,
+                android.R.layout.simple_list_item_2,
+                new String[]{"uname", "score"},
+                new int[]{android.R.id.text1, android.R.id.text2});
+
+
+
+      // adapter2.notifyDataSetChanged();
+
+
+       // adapter2.notifyDataSetChanged();
+
+          ListView l2 = (ListView) findViewById(R.id.listView2);
+        l2.setAdapter(adapter2);
+
+
+        Log.e("data2 sss",""+l2.getCount());
+
+        ListView l3 = (ListView) findViewById(R.id.listView3);
+        l3.setAdapter(adapter3);
+        ListView l4 = (ListView) findViewById(R.id.listView4);
+        l4.setAdapter(adapter4);
+        ListView l5 = (ListView) findViewById(R.id.listView5);
+        l5.setAdapter(adapter5);
+
+        try {
+            new LoadScore().execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        //Intent i = this.getIntent();
+        //xx = i.getStringExtra("user");
+
+
+
+//        helper = new HStoDB(this.getApplicationContext());
+//        SQLiteDatabase db = helper.getReadableDatabase();
+//        Cursor cursor = db.rawQuery("SELECT _id, ('Name : ' || name) n,( 'Score : ' || score) s FROM HighScore WHERE time = 15 ORDER BY score DESC LIMIT 0,3;", null);
+//        Cursor cursor2 = db.rawQuery("SELECT _id, ('Name : ' || name) n,( 'Score : ' || score) s FROM HighScore WHERE time = 30 ORDER BY score DESC LIMIT 0,3;", null);
+//        Cursor cursor3 = db.rawQuery("SELECT _id, ('Name : ' || name) n,( 'Score : ' || score) s FROM HighScore WHERE time = 45 ORDER BY score DESC LIMIT 0,3;", null);
+//        Cursor cursor4 = db.rawQuery("SELECT _id, ('Name : ' || name) n,( 'Score : ' || score) s FROM HighScore WHERE time = 60 ORDER BY score DESC LIMIT 0,3;", null);
+//
+//        adapter = new SimpleCursorAdapter(this,
+//                android.R.layout.simple_list_item_2,
+//                cursor,
+//                new String[]{"n", "s"},
+//                new int[]{android.R.id.text1, android.R.id.text2},
+//                0);
+//        adapter2 = new SimpleCursorAdapter(this,
+//                android.R.layout.simple_list_item_2,
+//                cursor2,
+//                new String[]{"n", "s"},
+//                new int[]{android.R.id.text1, android.R.id.text2},
+//                0);
+//        adapter3 = new SimpleCursorAdapter(this,
+//                android.R.layout.simple_list_item_2,
+//                cursor3,
+//                new String[]{"n", "s"},
+//                new int[]{android.R.id.text1, android.R.id.text2},
+//                0);
+//        adapter4 = new SimpleCursorAdapter(this,
+//                android.R.layout.simple_list_item_2,
+//                cursor4,
+//                new String[]{"n", "s"},
+//                new int[]{android.R.id.text1, android.R.id.text2},
+//                0);
+//
+//        ListView lv2 = (ListView) findViewById(R.id.listView2);
+//        lv2.setAdapter(adapter);
+//        ListView lv3 = (ListView) findViewById(R.id.listView3);
+//        lv3.setAdapter(adapter2);
+//        ListView lv4 = (ListView) findViewById(R.id.listView4);
+//        lv4.setAdapter(adapter3);
+//        ListView lv5 = (ListView) findViewById(R.id.listView5);
+//        lv5.setAdapter(adapter4);
+//        LoadScore task = new LoadScore();
+//        task.execute();
+    }
+
 
     class LoadScore extends AsyncTask<String, Void, Boolean> {
         @Override
@@ -68,23 +185,27 @@ public class HighScore extends ActionBarActivity {
                         String username = msg.getJSONObject(i).getString("uname");
                         String score = msg.getJSONObject(i).getString("score");
                         String scoretype = msg.getJSONObject(i).getString("time");
-                        String score_combine = "Score: " + score + "  ScoreType: " + scoretype;
+                        String score_combine = "Score: " + score;// + "  ScoreType: " + scoretype;
                         Map<String, String> item = new HashMap<String, String>();
                         item.put("uname", username);
                         item.put("score", score_combine);
 
                         switch (Integer.parseInt(scoretype)){
                             case 15:
-                                data2.add(0, item);
+                                data2.add(item);
+                                Log.e("data2",""+username);
+                                Log.e("data2",""+score_combine);
                                 break;
                             case 30:
-                                data3.add(0, item);
+                                data3.add(item);
+                                Log.e("data3",""+username);
+                                Log.e("data3",""+score_combine);
                                 break;
                             case 45:
-                                data4.add(0,item);
+                                data4.add(item);
                                 break;
                             case 60:
-                                data5.add(0,item);
+                                data5.add(item);
                                 break;
                         }
                     }
@@ -99,43 +220,58 @@ public class HighScore extends ActionBarActivity {
             }
             return false;
         }
-        /*
+
         @Override
-        protected Boolean doInBackground(String... params) {
-            BufferedReader reader;
-            StringBuilder buffer = new StringBuilder();
-            String line;
+        protected void onPostExecute(Boolean aBoolean) {
+           // super.onPostExecute(aBoolean);
 
-            try {
-                JSONObject jo = jp.getJSONFromUrl("http://ict.siit.tu.ac.th/~u5522791169/ITS333/testfetch.php");
-                JSONArray msg = jo.getJSONArray("msg");
-                for (int i = 0; i < msg.length(); i++) {
 
-                    String username = msg.getJSONObject(i).getString("uname");
-                    String score = msg.getJSONObject(i).getString("score");
-                    String scoretype = msg.getJSONObject(i).getString("time");
-                    String score_combine = "Score: " + score + "  ScoreType: " + scoretype;
-                    Map<String, String> item = new HashMap<String, String>();
-                    item.put("uname", username);
-                    item.put("score", score_combine);
-
-                    if(scoretype=="15"){
-                        data2.add(0,item);
-                    } else if(scoretype=="30"){
-                        data3.add(0,item);
-                    } else if(scoretype=="45"){
-                        data4.add(0,item);
-                    } else {
-                        data5.add(0,item);
-                    }
-                }
-                return true;
-            } catch (JSONException e) {
-                Log.e("LoadMessageTask", "Invalid JSON");
+            Log.e("data2 sss",""+data2);            //if (aBoolean)
+            {
+                adapter2.notifyDataSetChanged();
+                adapter3.notifyDataSetChanged();
+                adapter4.notifyDataSetChanged();
+                adapter5.notifyDataSetChanged();
             }
-            return false;
         }
-        */
+
+
+//        @Override
+//        protected Boolean doInBackground(String... params) {
+//            BufferedReader reader;
+//            StringBuilder buffer = new StringBuilder();
+//            String line;
+//
+//            try {
+//                JSONObject jo = jp.getJSONFromUrl("http://ict.siit.tu.ac.th/~u5522791169/ITS333/testfetch.php");
+//                JSONArray msg = jo.getJSONArray("msg");
+//                for (int i = 0; i < msg.length(); i++) {
+//
+//                    String username = msg.getJSONObject(i).getString("uname");
+//                    String score = msg.getJSONObject(i).getString("score");
+//                    String scoretype = msg.getJSONObject(i).getString("time");
+//                    String score_combine = "Score: " + score + "  ScoreType: " + scoretype;
+//                    Map<String, String> item = new HashMap<String, String>();
+//                    item.put("uname", username);
+//                    item.put("score", score_combine);
+//
+//                    if(scoretype=="15"){
+//                        data2.add(0,item);
+//                    } else if(scoretype=="30"){
+//                        data3.add(0,item);
+//                    } else if(scoretype=="45"){
+//                        data4.add(0,item);
+//                    } else {
+//                        data5.add(0,item);
+//                    }
+//                }
+//                return true;
+//            } catch (JSONException e) {
+//                Log.e("LoadMessageTask", "Invalid JSON");
+//            }
+//            return false;
+//        }
+
 
     }
 
@@ -190,96 +326,11 @@ public class HighScore extends ActionBarActivity {
 //        }
 //    }
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.highscore);
 
-        data2 = new ArrayList<Map<String, String>>();
-        data3 = new ArrayList<Map<String, String>>();
-        data4 = new ArrayList<Map<String, String>>();
-        data5 = new ArrayList<Map<String, String>>();
-        adapter2 = new SimpleAdapter(this,
-                data2,
-                android.R.layout.simple_list_item_2,
-                new String[]{"uname", "score"},
-                new int[]{android.R.id.text1, android.R.id.text2});
-        adapter3 = new SimpleAdapter(this,
-                data3,
-                android.R.layout.simple_list_item_2,
-                new String[]{"uname", "score"},
-                new int[]{android.R.id.text1, android.R.id.text2});
-        adapter4 = new SimpleAdapter(this,
-                data4,
-                android.R.layout.simple_list_item_2,
-                new String[]{"uname", "score"},
-                new int[]{android.R.id.text1, android.R.id.text2});
-        adapter5 = new SimpleAdapter(this,
-                data5,
-                android.R.layout.simple_list_item_2,
-                new String[]{"uname", "score"},
-                new int[]{android.R.id.text1, android.R.id.text2});
-        ListView l2 = (ListView) findViewById(R.id.listView2);
-        l2.setAdapter(adapter2);
-        ListView l3 = (ListView) findViewById(R.id.listView3);
-        l3.setAdapter(adapter3);
-        ListView l4 = (ListView) findViewById(R.id.listView4);
-        l4.setAdapter(adapter4);
-        ListView l5 = (ListView) findViewById(R.id.listView5);
-        l5.setAdapter(adapter5);
-        LoadScore task = new LoadScore();
-        task.execute();
-        Intent i = this.getIntent();
-        //user = i.getStringExtra("user");
-
-
-
-//        helper = new HStoDB(this.getApplicationContext());
-//        SQLiteDatabase db = helper.getReadableDatabase();
-//        Cursor cursor = db.rawQuery("SELECT _id, ('Name : ' || name) n,( 'Score : ' || score) s FROM HighScore WHERE time = 15 ORDER BY score DESC LIMIT 0,3;", null);
-//        Cursor cursor2 = db.rawQuery("SELECT _id, ('Name : ' || name) n,( 'Score : ' || score) s FROM HighScore WHERE time = 30 ORDER BY score DESC LIMIT 0,3;", null);
-//        Cursor cursor3 = db.rawQuery("SELECT _id, ('Name : ' || name) n,( 'Score : ' || score) s FROM HighScore WHERE time = 45 ORDER BY score DESC LIMIT 0,3;", null);
-//        Cursor cursor4 = db.rawQuery("SELECT _id, ('Name : ' || name) n,( 'Score : ' || score) s FROM HighScore WHERE time = 60 ORDER BY score DESC LIMIT 0,3;", null);
-//
-//        adapter = new SimpleCursorAdapter(this,
-//                android.R.layout.simple_list_item_2,
-//                cursor,
-//                new String[]{"n", "s"},
-//                new int[]{android.R.id.text1, android.R.id.text2},
-//                0);
-//        adapter2 = new SimpleCursorAdapter(this,
-//                android.R.layout.simple_list_item_2,
-//                cursor2,
-//                new String[]{"n", "s"},
-//                new int[]{android.R.id.text1, android.R.id.text2},
-//                0);
-//        adapter3 = new SimpleCursorAdapter(this,
-//                android.R.layout.simple_list_item_2,
-//                cursor3,
-//                new String[]{"n", "s"},
-//                new int[]{android.R.id.text1, android.R.id.text2},
-//                0);
-//        adapter4 = new SimpleCursorAdapter(this,
-//                android.R.layout.simple_list_item_2,
-//                cursor4,
-//                new String[]{"n", "s"},
-//                new int[]{android.R.id.text1, android.R.id.text2},
-//                0);
-//
-//        ListView lv2 = (ListView) findViewById(R.id.listView2);
-//        lv2.setAdapter(adapter);
-//        ListView lv3 = (ListView) findViewById(R.id.listView3);
-//        lv3.setAdapter(adapter2);
-//        ListView lv4 = (ListView) findViewById(R.id.listView4);
-//        lv4.setAdapter(adapter3);
-//        ListView lv5 = (ListView) findViewById(R.id.listView5);
-//        lv5.setAdapter(adapter4);
-//        LoadScore task = new LoadScore();
-//        task.execute();
-   }
 
     protected void onResume(){
         super.onResume();
-        setContentView(R.layout.highscore);
+     //   setContentView(R.layout.highscore);
 
 //        helper = new HStoDB(this.getApplicationContext());
 //        SQLiteDatabase db = helper.getReadableDatabase();
